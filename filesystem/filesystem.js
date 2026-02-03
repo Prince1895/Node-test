807693d8d3b375fca8227cc0300d1fce290a30c8
Prince Kumar <chauhanprince21153366@gmail.com>
2026-02-03 11:56:49 +0530

    async /sync -  read/write

--- filesystem/filesystem.js
+++ filesystem/filesystem.js
@@ -1,5 +1,19 @@
 const fs=require('fs');
 // fs.writeFileSync("./ds-b.txt","Hello Abes college ")
 // fs.writeFileSync("./ABES.txt","Hii Abes college \n Hii Abes college \n Hii Abes college  ")
-console.log(fs.readFileSync("./ABES.txt","utf-8"))
-console.log(fs.readFileSync("./ds-b.txt","utf-8"))
+// console.log(fs.readFileSync("./ABES.txt","utf-8"))
+// console.log(fs.readFileSync("./ds-b.txt","utf-8"))
+// fs.writeFile("./abes.txt","we code in java",()=>{});
+// fs.writeFileSync("./A1.txt","Hello Abes college ")
+// fs.writeFile("./A1.txt","Hello Abes college ",()=>{})
+// fs.readFileSync("./A1.txt","utf-8")
+// fs.readFile("./A1.txt","utf-8",()=>{})
+// fs.readFile("./ds-b.txt","utf-8",(err,result)=>{
+// if(err){
+//     console.log("Error",err);
+// }
+// else{
+//     console.log(result);
+// }
+// })
+
