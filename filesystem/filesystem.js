eddec1a47b319a46d6d9aa0f5a6888bd60e96c6f
Prince Kumar <chauhanprince21153366@gmail.com>
2026-02-17 11:29:48 +0530

    os

--- filesystem/filesystem.js
+++ filesystem/filesystem.js
@@ -46,4 +46,6 @@ console.log("System platform", os.platform());
 console.log("user info",os.userInfo());
 console.log("cpu",os.arch());
 console.log("free memory",os.freemem());
+console.log("total memory",os.totalmem());
+console.log("uptime",os.uptime());
 
